978262b77e53164b17da68685258d8a5d53fdada
cesarcastro55 <cesarnscastro@gmail.com>
2021-05-29 20:58:13 +0100

    fase 3, sabado

--- app/src/main/java/com/example/moneyinc/SelectAccountFragment.kt
+++ app/src/main/java/com/example/moneyinc/SelectAccountFragment.kt
@@ -7,10 +7,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.moneyinc.databinding.FragmentSelectAccountBinding
+import kotlinx.android.synthetic.main.fragment_select_account.*
 import retrofit2.Call
 import retrofit2.Response
 
@@ -19,6 +22,8 @@ class SelectAccountFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    private val adapter = CustomAdapter(listOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,55 +37,15 @@ class SelectAccountFragment : Fragment() {
             false
         )
 
-
         val aux: SelectAccountFragmentArgs ?= arguments?.let { SelectAccountFragmentArgs.fromBundle(it) }
         val args = aux?.token.toString()
         val token = "token $args"
         Log.e("Token recebido!!", token)
 
         var page = "1"
-        var id: String
-        var type: String
-        var titular1: String
-        var titular2: String
-        var titular3: String
-        var iban: String
-        var nib: String
-        var swift: String
-        var active: String
-        var approved: String
-        var createdOn: String
-        var saldo: String
 
         Log.d("aqui", page)
 
-        fun getAccountList(token: String, page: String) {
-
-            ServiceApi3.retrofitService.getLista(token,page).enqueue(
-                object : retrofit2.Callback<Lista> {
-                    override fun onResponse(call: Call<Lista>, response: Response<Lista>) {
-
-                        Log.e("sucesso", " com dados1!!")
-                        for(i in response.body()?.results.orEmpty()){
-                            titular1 = i.titular1
-                            id = i.id.toString()
-                            type = i.type
-                            iban = i.iban
-                            Log.d("titular", titular1)
-                            Log.d("id", id)
-                        }
-                        //if(response.code() == 404) page = "1"  // quando nao houver mais paginas, voltar à pag1
-                    }
-
-                    override fun onFailure(call: Call<Lista>, t: Throwable) {
-                        Log.e("Erro!!", "Sem dados3!!")
-                        //Log.d("aquiFailure", t.message.toString())
-                    }
-
-                }
-            )
-
-        }
         getAccountList(token, page)
         binding.button2.setOnClickListener {
             val aux: NavDirections = SelectAccountFragmentDirections.actionSelectAccountFragmentToHomeFragment(token)
@@ -100,6 +65,44 @@ class SelectAccountFragment : Fragment() {
             getAccountList(token, page)
         }
 
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = adapter
+
         return binding.root
     }
+
+    private fun getAccountList(token: String, page: String) {
+
+        ServiceApi3.retrofitService.getLista(token,page).enqueue(
+            object : retrofit2.Callback<Lista> {
+                override fun onResponse(call: Call<Lista>, response: Response<Lista>) {
+                    val lista = mutableListOf<UserInfo>()
+                    for (user in response.body()?.results.orEmpty()) {
+                        val item = UserInfo(user.id, user.type, user.titular1, user.titular2, user.titular3,
+                            user.iban, user.nib, user.swift, user.active, user.approved, user.createdOn, user.saldo)
+                        lista.add(item)
+                    }
+                    //passar lista ao adapter
+                    adapter.reloadItems(lista)
+                }
+
+                override fun onFailure(call: Call<Lista>, t: Throwable) {
+                    Log.e("Erro!!", "Sem dados3!!")
+                    //Log.d("aquiFailure", t.message.toString())
+                }
+
+            }
+        )
+
+    }
+
+    /*private fun generateList(size: Int): List<UserInfo>{
+        val list = ArrayList<UserInfo>()
+
+        for (i in 0 until size){
+            list[i] =
+        }
+    }*/
+
+
 }
